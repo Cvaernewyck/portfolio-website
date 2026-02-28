8fc732ccc8a3dd4923d1c049b6dbf9389647c1d9
Cedric <cedric_vaernewyck@epam.com>
2026-02-28 10:07:22 +0100

    cheap page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,9 +17,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/cheap" element={<Cheap />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Route path="/cheap" element={<Cheap />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
